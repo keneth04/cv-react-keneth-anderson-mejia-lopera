abc6cce7166994857f7c37ff171df34bf1cd1ed3
Keneth Lopera <kenethlopera@gmail.com>
2025-11-14 14:08:06 -0500

    feat: componente Perfil con descripción personalizada

---
+++ src/components/perfil.jsx
@@ -0,0 +1,20 @@
+export default function Perfil() {
+    return (
+        <section>
+        <h3>Perfil</h3>
+        <p>
+            Desarrollador Front-End en formación con conocimientos en React y Vite. 
+            Experiencia en creación de interfaces modulares y responsive, interés en buenas prácticas, control de versiones y accesibilidad.
+        </p>
+
+        <h4 style={{marginTop:12}}>Habilidades</h4>
+        <ul>
+            <li>React (componentes funcionales, hooks)</li>
+            <li>JavaScript moderno (ES6+)</li>
+            <li>HTML5, CSS3 (Responsive)</li>
+            <li>Git & GitHub (control de versiones)</li>
+            <li>Comunicación y trabajo en equipo</li>
+        </ul>
+        </section>
+    );
+    }
